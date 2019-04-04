c1571b9452b801d149790b99cbdebc36174e5308
Faysal Ahamed <miufaysal@live.com>
2019-04-04 05:50:24 +0000

    Apply fixes from StyleCI

--- tests/RedirectTest.php
+++ tests/RedirectTest.php
@@ -6,14 +6,9 @@ use Orchestra\Testbench\TestCase;
 
 class RedirectTest extends TestCase
 {
-
     /** @test */
-    
     public function true_is_true()
     {
-
         $this->assertTrue(true);
-
     }
-
 }
