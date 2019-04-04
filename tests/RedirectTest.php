9a90917e38cadf7410a2f72237bd97198d8f9286
Faysal Ahamed <miufaysal@live.com>
2019-04-04 11:41:08 +0600

    Test

--- tests/RedirectTest.php
+++ tests/RedirectTest.php
@@ -2,13 +2,18 @@
 
 namespace Devfaysal\LaravelDomainRedirect\Tests;
 
-use PHPUnit\Framework\TestCase;
+use Orchestra\Testbench\TestCase;
 
-class ExampleTest extends TestCase
+class RedirectTest extends TestCase
 {
+
     /** @test */
+    
     public function true_is_true()
     {
+
         $this->assertTrue(true);
+
     }
+
 }
